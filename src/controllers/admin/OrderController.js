765a96622239a55b6cc3b374da48256738d9ae76
soumya453 <biswassoumya97@gmail.com>
2025-06-04 22:34:14 +0530

    code updated

---
+++ src/controllers/admin/OrderController.js
@@ -0,0 +1,59 @@
+import { Router } from "express";
+import { orderModel } from "../../models/Order.js";
+export default class UserController{
+    constructor(){
+        const route = Router();
+        route.get('/', this.list);
+        route.get('/show/:id', this.show);
+        route.get('/create', this.create);
+        route.post('/', this.save);
+        route.get('/edit/:id', this.edit);
+        route.post('/update', this.update);
+        route.post('/delete/:id', this.delete);
+        return route;
+    }
+
+    async list(req, res){
+        const orders = await orderModel.all();
+        res.render('admin/orders/index', {orders, title: 'Orders Section'});
+    }
+
+    async show(req, res){
+        const order = await orderModel.find(req.params.id);
+        res.render('admin/orders/show', {order, title: 'Order Details'});
+    }
+
+    async create(req, res){
+        res.render('admin/orders/create', {title: 'Create Order'});
+    }
+
+    async save(req, res){
+        const order = req.body;
+        const img = req.file;
+        console.log(img);
+        
+        delete order.image;
+        await orderModel.create(order);
+        res.redirect('/admin/orders');
+    }
+
+    async edit(req, res){
+        const order = await orderModel.find(req.params.id);
+        res.render('admin/orders/edit', {order, title: 'Edit Order'});
+    }
+
+    async update(req, res){
+        const order = req.body;
+        const img = req.file;
+        console.log(img);
+        delete order.image;
+        await orderModel.update(order.id, order);
+        res.redirect('/admin/orders');
+    }
+
+    async delete(req, res){
+        await orderModel.delete(req.params.id);
+        res.redirect('/admin/orders');
+    }
+
+}
